348f9a063939583ec290e9164b7ecd0e02911a46
alex-dmb <alex7dmb@gmail.com>
2022-07-05 14:15:32 -0300

    swiper added in graphic design page

---
+++ scripts.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".swiper", {
+    slidesPerView: 3,
+    spaceBetween: 0,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  
