7544d90b034c77037a3cc5603150fa04a2e3509e
alex-dmb <alex7dmb@gmail.com>
2022-07-11 19:10:17 -0300

    typing effect in my name

--- scripts.js
+++ scripts.js
@@ -1,8 +1,32 @@
+/* Light Mode */
+
 const toggleButton = document.getElementById("toggle");
 
 toggleButton.addEventListener("click", function(){
   document.body.classList.toggle("light-mode");
 })
+
+/* Efeito Digitação */
+
+function typeWriter(element){
+  const arrayText = element.innerHTML.split('');
+  element.innerHTML = ' ';
+  arrayText.forEach(function(letter, i){   
+    
+  setTimeout(function(){
+      element.innerHTML += letter;
+  }, 200 * i)
+
+});
+}
+
+
+const name = document.getElementById('myName');
+typeWriter(name);
+
+
+
+/* Scroll Reveal */
 
 ScrollReveal().reveal('.scrollReveal',{ duration: 1500, reset: false, distance: '120px' } );
 
